56076168243e07c83e3276c4407c42f277f46768
Yusuke Saito <ysaito8015@gmail.com>
2023-02-04 21:52:36 +0900

    amend indent

--- ch02/encode_swe.py
+++ ch02/encode_swe.py
@@ -73,27 +73,27 @@ class SWEEncoder_ja:
         while pos < len(text):
             end = min(len(text), pos+self.maxlen+1) if text[pos] == '<' else pos+3
             kouho = []
-        for e in range(end, pos, -1):
-            wd = text[pos:e]
-            if wd in self.swe:
-                if wd[0] == '<' and len(wd) > 2:
-                    kouho = [(self.swe[wd], e)]
-                    break
-                else:
-                    kouho.append((self.swe[wd], e))
-        if len(kouho) > 0:
-            wp,  e = sorted(kouho, key=lambda x: x[0])[0]
-            result.append(wp)
-            pos = e
-        else:
-            end = pos+1
-            wd = text[pos:end]
-            if checkkigou(wd):
-                result.append(self.swe['<KIGOU>'])
-            elif checku2e(wd):
-                result.append(self.swe['<U2000U2BFF>'])
+            for e in range(end, pos, -1):
+                wd = text[pos:e]
+                if wd in self.swe:
+                    if wd[0] == '<' and len(wd) > 2:
+                        kouho = [(self.swe[wd], e)]
+                        break
+                    else:
+                        kouho.append((self.swe[wd], e))
+            if len(kouho) > 0:
+                wp,  e = sorted(kouho, key=lambda x: x[0])[0]
+                result.append(wp)
+                pos = e
             else:
-                for i in wd.encode('utf-8'):
-                    result.append(self.swe['<|byte%d|>' % i])
-            pos = end
+                end = pos+1
+                wd = text[pos:end]
+                if checkkigou(wd):
+                    result.append(self.swe['<KIGOU>'])
+                elif checku2e(wd):
+                    result.append(self.swe['<U2000U2BFF>'])
+                else:
+                    for i in wd.encode('utf-8'):
+                        result.append(self.swe['<|byte%d|>' % i])
+                pos = end
         return result
